501b5969d70887d8acdab31bb9458d9be1150741
TonyStinson1 <pankajswami69@gmail.com>
2023-04-21 16:50:44 +0530

    Add send mail functionality

--- Src/Screen/Walletscreen/Certificate.js
+++ Src/Screen/Walletscreen/Certificate.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useRef, useState} from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import {
   View,
   Text,
@@ -8,11 +8,13 @@ import {
   ActivityIndicator,
   ImageBackground,
   Image,
+  Modal
 } from 'react-native';
+import Toast from 'react-native-toast-message';
 
 import Carousel from 'react-native-snap-carousel';
-import {COLOR} from '../../Constant/color';
-import {proof} from '../../Constant/json';
+import { COLOR } from '../../Constant/color';
+import { proof } from '../../Constant/json';
 import moment from 'moment';
 import {
   Header as HeaderRNE,
@@ -20,13 +22,13 @@ import {
   Icon,
   BottomSheet,
 } from '@rneui/themed';
-import {useSelector, useDispatch} from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 
 import QRCode from 'react-native-qrcode-svg';
 import axiosInstance from '../../Constant/axios';
-import { Toast } from 'react-native-toast-message';
-import {sendToverification} from '../../Function/Apicall';
-import {useNavigation} from '@react-navigation/native';
+import { createProofforOR, sendToverification } from '../../Function/Apicall';
+import { useNavigation } from '@react-navigation/native';
+import { TextInput } from 'react-native-gesture-handler';
 
 const WIDTH = Dimensions.get('screen').width;
 
@@ -47,6 +49,14 @@ const Certificate = ({
   const [emailApproval, setEmailapproval] = useState(false);
   const [emailIcon, setEmailicon] = useState([]);
   const [apiloader, setApiloader] = useState(false);
+  const [dropdown, setDropdown] = useState('')
+  const [modal, setModal] = useState(false)
+  const [email, setEmail] = useState('')
+  const [message, setMessage] = useState('')
+  const [DIDid, setDIDid] = useState('')
+  const [DIDiv, setDIDiv] = useState('')
+  const [submitloader, setSubmitloader] = useState(false)
+  const profileData = useSelector(state => state?.appstate?.profileData);
 
   const ref = useRef(null);
 
@@ -55,7 +65,7 @@ const Certificate = ({
   const cardData = useSelector(state => state.appstate.cardList);
 
   const createProof = async id => {
-    let dataToSend = {item_id: id};
+    let dataToSend = { item_id: id };
 
     axiosInstance
       .post('auth/create-proof-qr', dataToSend)
@@ -78,10 +88,10 @@ const Certificate = ({
           topOffset: 100,
           type: "error",
           text1: "ERROR",
-         text2: `Something went wrong , Please try again`,
+          text2: `Something went wrong , Please try again`,
           visibilityTime: 3000,
           props: {
-            text1NumberOfLines:2 //number of how many lines you want
+            text1NumberOfLines: 2 //number of how many lines you want
           }
         });
       });
@@ -101,7 +111,7 @@ const Certificate = ({
   const handlemailicon = async id => {
     setApiloader(true);
 
-    let dataToSend = {item_id: id};
+    let dataToSend = { item_id: id };
 
     axiosInstance
       .post('auth/create-proof-qr', dataToSend)
@@ -131,10 +141,10 @@ const Certificate = ({
           topOffset: 100,
           type: "error",
           text1: "ERROR",
-         text2: `Something went wrong , Please try again`,
+          text2: `Something went wrong , Please try again`,
           visibilityTime: 3000,
           props: {
-            text1NumberOfLines:2 //number of how many lines you want
+            text1NumberOfLines: 2 //number of how many lines you want
           }
         });
       });
@@ -152,7 +162,68 @@ const Certificate = ({
     }
   };
 
-  const renderItem = ({item, index}) => {
+  const hndlecancle = () => {
+    setModal(false)
+    setDropdown('')
+  }
+
+  const handlesendmail = async () => {
+    setModal(true)
+    const id = dropdown
+    const result = await createProofforOR(id);
+
+    const resid = result?.id;
+    const resiv = result?.iv;
+    setDIDid(resid)
+    setDIDiv(resiv)
+
+  }
+
+  const sendmail = async () => {
+    setSubmitloader(true)
+
+    let dataToSend = {
+      "to": email,
+      "data": {
+        "type": "verification-email",
+        "email": profileData?.email
+      },
+      "message": message,
+      "askedFor": ["DID"],
+      "did": [{ "id": DIDid, "iv": DIDiv, "name": "DID" }]
+    };
+
+    axiosInstance
+      .post('mail/sendSingleDid', dataToSend)
+      .then(async function (responseJson) {
+        if (responseJson.status === 200) {
+          console.log("$$$$$$", responseJson.data?.err);
+          setModal(false)
+          setDropdown(false)
+          setEmail('')
+          setMessage('')
+          setDIDid('')
+          setDIDiv('')
+          setSubmitloader(false)
+        }
+      })
+      .catch(function (error) {
+        Toast.show({
+          topOffset: 100,
+          type: "error",
+          text1: "ERROR",
+          text2: `Something went wrong , Please try again`,
+          visibilityTime: 3000,
+          props: {
+            text1NumberOfLines: 2 //number of how many lines you want
+          }
+
+        });
+        setSubmitloader(false)
+      });
+  }
+
+  const renderItem = ({ item, index }) => {
     const insdate = item?.data?.issuanceDate;
     const memberdate = item?.data?.proof?.created;
 
@@ -164,29 +235,30 @@ const Certificate = ({
     const id = item?.id;
 
     const emailid = emailIcon.findIndex(value => value === id);
+    // console.log("%%%%%", item, id);
 
     if (loader) {
       return (
-        <View style={{width: '80 %', height: 150}}>
+        <View style={{ width: '80 %', height: 150 }}>
           <ActivityIndicator size="large" color="#00ff00" />
         </View>
       );
     }
 
     return (
-      <View style={{width: '100%', height: 300, flexDirection: 'row'}}>
-       {check && 
-        <TouchableOpacity
-          onPress={() => {
-            handleproof(id);
-          }}
-          style={{
-            width: 25,
-            height: 25,
-            marginTop: 60,
-            borderRadius: 25 / 2,
-            backgroundColor: 'gray',
-          }}></TouchableOpacity>
+      <View style={{ width: '100%', height: 300, flexDirection: 'row' }}>
+        {check &&
+          <TouchableOpacity
+            onPress={() => {
+              handleproof(id);
+            }}
+            style={{
+              width: 25,
+              height: 25,
+              marginTop: 60,
+              borderRadius: 25 / 2,
+              backgroundColor: 'gray',
+            }}></TouchableOpacity>
         }
         {/* {toMail && toMail.length > 0 &&
           <View style={{ alignItems: "flex-start", marginBottom: -5, marginLeft: 25, }}>
@@ -199,10 +271,10 @@ const Certificate = ({
           </View>
         } */}
 
-        <View style={{flex: 1, width: 320, height: 250}}>
+        <View style={{ flex: 1, width: 320, height: 250 }}>
           <Image
             source={imgbkg}
-            style={{width: '100%', height: '100%', resizeMode: 'stretch'}}
+            style={{ width: '100%', height: '100%', resizeMode: 'stretch' }}
           />
           <View
             style={{
@@ -221,7 +293,7 @@ const Certificate = ({
                 justifyContent: 'space-between',
               }}>
               <TouchableOpacity
-                onPress={() => navigation.navigate('Postauth' ,{screen: 'Cardinfo',params :{ data: item}})}>
+                onPress={() => navigation.navigate('Postauth', { screen: 'Cardinfo', params: { data: item } })}>
                 <Icon
                   name="information-circle-outline"
                   type="ionicon"
@@ -254,14 +326,14 @@ const Certificate = ({
                 justifyContent: 'center',
               }}>
               <View
-                style={{flexDirection: 'row', alignContent: 'space-between'}}>
-                <View style={{width: '50%'}}>
+                style={{ flexDirection: 'row', alignContent: 'space-between' }}>
+                <View style={{ width: '50%' }}>
                   <Text
-                    style={{fontWeight: '600', color: '#FFFFFF', fontSize: 20}}>
+                    style={{ fontWeight: '600', color: '#FFFFFF', fontSize: 20 }}>
                     UID
                   </Text>
                   <Text
-                    style={{fontWeight: '600', color: '#FFFFFF', fontSize: 12}}>
+                    style={{ fontWeight: '600', color: '#FFFFFF', fontSize: 12 }}>
                     {item.id.split(':')[2].slice(0, 6)}...
                     {item.id.split(':')[2].slice(24, 32)}
                   </Text>
@@ -283,7 +355,7 @@ const Certificate = ({
                       height: 40,
                     }}
                     onPress={() => openmodal(item?.id)}>
-                    <Text style={{fontWeight: '600', color: 'black'}}>
+                    <Text style={{ fontWeight: '600', color: 'black' }}>
                       SHARE
                     </Text>
                   </TouchableOpacity>
@@ -297,10 +369,32 @@ const Certificate = ({
                       borderBottomRightRadius: 25,
                       borderTopRightRadius: 25,
                       height: 40,
-                    }}>
+                    }} onPress={() => dropdown.length > 0 ? dropdown === id ? setDropdown('') : setDropdown(id) : setDropdown(id)}>
                     <Icon name="chevron-small-down" type="entypo" size={35} />
                   </TouchableOpacity>
                 </View>
+                {dropdown === id &&
+                  <View style={{
+                    marginTop: 45,
+                    backgroundColor: "white",
+                    position: "absolute",
+                    right: 10,
+                    elevation: 5,
+                    width: 110,
+                    borderTopLeftRadius: 10,
+                    borderBottomLeftRadius: 10,
+                    borderBottomRightRadius: 10,
+                    borderTopRightRadius: 10,
+                    alignItems: "center",
+                    justifyContent: "center",
+
+                    height: 40,
+                  }}>
+                    <TouchableOpacity onPress={() => handlesendmail()}>
+                      <Text style={{ color: COLOR.BLUE[300], fontWeight: "bold", fontSize: 15 }}>Send Mail</Text>
+                    </TouchableOpacity>
+                  </View>
+                }
               </View>
             </View>
           </View>
@@ -319,9 +413,9 @@ const Certificate = ({
         justifyContent: 'center',
       }}>
       {cardData && cardData?.length < 1 ? (
-        <View style={{height: '100%', alignItems: 'center', width: '100%'}}>
+        <View style={{ height: '100%', alignItems: 'center', width: '100%' }}>
           <Text
-            style={{fontSize: 25, color: COLOR.BLACK[100], fontWeight: '700'}}>
+            style={{ fontSize: 25, color: COLOR.BLACK[100], fontWeight: '700' }}>
             You don't have any wallet data
           </Text>
           <TouchableOpacity
@@ -355,7 +449,7 @@ const Certificate = ({
             paddingLeft: 10,
           }}>
           {cardData.map((item, i) => {
-            return renderItem({item});
+            return renderItem({ item });
           })}
         </View>
         // <Carousel
@@ -450,6 +544,59 @@ const Certificate = ({
           </View>
         </TouchableOpacity>
       </BottomSheet>
+
+      <Modal
+        transparent={true}
+        animationType={'none'}
+        visible={modal}
+        onRequestClose={() => {
+          setModal(false)
+        }}>
+        <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+          <View style={{ width: "80%", backgroundColor: "white", borderRadius: 15, padding: 15, elevation: 15 }}>
+            <Text style={{ color: COLOR.BLUE[500], fontSize: 18, fontWeight: "bold", marginLeft: 10 }}>Email ID</Text>
+            <TextInput placeholder='Enter Email Address'
+              placeholderTextColor={"gray"}
+              value={email}
+              onChangeText={(text) => setEmail(text)}
+              style={{ borderWidth: 0.5, borderRadius: 10, marginTop: 5, paddingHorizontal: 15, borderColor: COLOR.GRAY[100] }} />
+
+            <Text style={{ color: COLOR.BLUE[500], fontSize: 18, fontWeight: "bold", marginLeft: 10, marginTop: 15 }}>Message</Text>
+            <TextInput placeholder='Email Id'
+              placeholderTextColor={"gray"}
+              value={message}
+              onChangeText={(text) => setMessage(text)}
+              style={{ borderWidth: 0.5, borderRadius: 10, marginTop: 5, paddingHorizontal: 15, borderColor: COLOR.GRAY[100] }} />
+
+            <TouchableOpacity style={{
+              width: "70%",
+              alignSelf: "center",
+              backgroundColor: COLOR.BLUE[500],
+              marginTop: 20,
+              alignItems: "center", justifyContent: "center", paddingVertical: 10, borderRadius: 15
+            }} onPress={() => sendmail()}>
+              {submitloader ? (
+                <ActivityIndicator size="small" color="white" />
+              ) : (
+                <Text style={{ color: "white", fontSize: 18, fontWeight: "bold" }}>Submit</Text>
+              )}
+
+
+            </TouchableOpacity>
+
+            <TouchableOpacity style={{
+              width: "70%",
+              alignSelf: "center",
+              backgroundColor: "red",
+              marginTop: 10,
+              alignItems: "center", justifyContent: "center", paddingVertical: 10, borderRadius: 15
+            }} onPress={() => hndlecancle()}>
+              <Text style={{ color: "white", fontSize: 18, fontWeight: "bold" }}>Cancle
+              </Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </Modal>
     </View>
   );
 };
